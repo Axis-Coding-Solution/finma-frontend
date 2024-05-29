f4626566a52368b148d6e54b46b2e0ea68d8a48b
Huzaifa Khan <99420348+Zalmi-Khan@users.noreply.github.com>
2024-05-29 11:30:27 +0500

    make layout of onboarding

--- src/pages/onboarding/start-onboarding/index.tsx
+++ src/pages/onboarding/start-onboarding/index.tsx
@@ -4,15 +4,17 @@ import { MainHeading } from '@/pages/components/common';
 import { Link } from 'react-router-dom';
 
 function StartOnboardingPage() {
-  return <div className="bg-background py-5 rounded-lg h-full flex justify-center items-center">
-    <div className='w-[60%] flex justify-center items-center'><img src={arrowImgPath} alt="" className='self-start block h-80 w-[300px]' />
-      <div className='-ml-20 mt-32'>
+  return <div className="bg-background   rounded-lg  flex justify-center items-center">
+    <div className='w-[60%] relative py-20'>
+      <img src={arrowImgPath} alt="" className='absolute top-0 left-0 w-60' />
+      <div className=''>
         <MainHeading
           heading='Welcome to the startup idea evaluation model!' paragraph='This tool will help you evaluate your startup idea based on key validation points. Follow the guidelines provided to input detailed information for each validation point. This will allow the model to assess the risk and potential of your idea. Let`s get started!' />
         <Link title="Start Onboarding" to="/onboarding/idea-clarity/problem">
           <Button variant="default" className='mt-5'>Get started</Button>
         </Link>
-      </div></div>
+      </div>
+    </div>
   </div >;
 }
 
