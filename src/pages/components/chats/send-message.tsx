0516371a05af1e453970170174c5f2dd00256330
hamza khalid <hamzakhalid.654@gmail.com>
2024-05-31 17:36:26 +0500

    set massagesend

--- src/pages/components/chats/send-message.tsx
+++ src/pages/components/chats/send-message.tsx
@@ -2,8 +2,8 @@ import { Paperclip, Send, SmilePlus } from "lucide-react";
 
 export const SendMessageBox = () => {
   return (
-    <div className="flex gap-1 pb-10">
-      <div className="flex justify-between w-[60%] rounded-sm h-10 px-3 py-2 border border-border">
+    <div className="flex sticky bottom-0 left-0 gap-1  w-full bg-background">
+      <div className="flex justify-between w-full rounded-sm h-10 px-3 py-2 border border-border">
         <div className="flex gap-2 ">
           <Paperclip size={20} />
           <h1>Write a Message</h1>
