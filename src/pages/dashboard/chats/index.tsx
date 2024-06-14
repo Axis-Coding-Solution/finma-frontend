0df9be01a9a3920c549dbc46e0dd82d4da9c2e89
hamza khalid <hamzakhalid.654@gmail.com>
2024-06-14 21:31:12 +0500

    clean code

--- src/pages/dashboard/chats/index.tsx
+++ src/pages/dashboard/chats/index.tsx
@@ -1,15 +1,9 @@
-import {
-  ChatContent,
-  ChatHeader,
-} from "@/pages/components/chats";
+import { ChatContent, ChatHeader } from "@/pages/components/chats";
 import { ChatSidebar } from "@/pages/components/chats/chat-sidebar";
 import { MainHeading } from "@/pages/components/common";
 
 function ChatBoxPage() {
-
-
   return (
-    
     <div className="grid grid-cols-12 gap-6 h-[calc(100vh-112px)]  overflow-hidden">
       <div className="col-span-12">
         <MainHeading heading="Chats" />
