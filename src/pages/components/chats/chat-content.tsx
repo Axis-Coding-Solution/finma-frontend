e23482a885a07e7d0f1bba712602044a536577d3
Farasat-Basra <chfrasatbasra@gmail.com>
2024-06-26 17:17:24 +0500

    fixes

--- src/pages/components/chats/chat-content.tsx
+++ src/pages/components/chats/chat-content.tsx
@@ -3,6 +3,7 @@ import { userAvatar2Image } from "@/assets/images";
 import { Avatar } from "@/components/ui/avatar";
 import { chatRoomData } from "@/lib/data";
 import ScrollBar from "react-perfect-scrollbar";
+import { TextMessage } from "./content/Text-Message";
 
 export const ChatContent = () => {
   // const [data, setData] = useState(null);
@@ -35,31 +36,11 @@ export const ChatContent = () => {
       <ScrollBar>
         <div className="flex flex-col px-5   h-[25rem] ">
           {chatRoomData.map((message, index) => (
-            <div
-              className={`flex  ${
-                message.sender ? "justify-start" : "justify-end"
-              } `}
-              key={index}
-            >
-              <div className="flex gap-2 items-end mb-2">
-                {message.sender === true && <Avatar image={userAvatar2Image} />}
-                <div
-                  className="px-5 py-2  min-w-[13rem]  max-w-[70%]"
-                  style={{
-                    border: "2px solid #E2E8F0",
-                    borderTopLeftRadius: "10px",
-                    borderTopRightRadius: "20px",
-                    borderBottomRightRadius: "20px",
-                    borderBottomLeftRadius: "0px",
-                  }}
-                >
-                  <h1>{message.content}</h1>
-                  <p className="text-end text-[11px] pt-[2px] text-muted-foreground ">
-                    {message.time}
-                  </p>
-                </div>
-              </div>
-            </div>
+            <TextMessage
+              message={message}
+              index={index}
+              position={message.sender ? "left" : "right"}
+            />
           ))}
         </div>
       </ScrollBar>
