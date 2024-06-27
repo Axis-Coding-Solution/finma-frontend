d1715adc592783227ce57d4e936466ee8e4531be
hamza khalid <hamzakhalid.654@gmail.com>
2024-06-27 17:52:05 +0500

    done

--- src/utils/initial-values/onboarding.ts
+++ src/utils/initial-values/onboarding.ts
@@ -10,44 +10,73 @@ export const userQuestionaryInitialValues = {
   financialStatus: "",
   familyStatus: "",
 };
-export const onboardingExpertsInitialValues = {
-  firstName: "",
-  lastName: "",
-  country: "",
-  city: "",
-  myCurrentPosition: "",
-  areaOfExpertise: "",
-  whatICanDo: "",
-  myGoalsIsTo: "",
-  workCompensationsOptions: "",
-  projectSelectionProcess: "",
-  projectIntakeProcess: "",
-  projectEngagementPreference: "",
-  currency: "",
-  discoveryCall: "",
-  consultation: "",
-  delivery: "",
-  profilePicture: null,
-};
+// export const onboardingExpertsInitialValues = {
+//   firstName: "",
+//   lastName: "",
+//   country: "",
+//   city: "",
+//   myCurrentPosition: "",
+//   areaOfExpertise: "",
+//   whatICanDo: "",
+//   myGoalsIsTo: "",
+//   workCompensationsOptions: "",
+//   projectSelectionProcess: "",
+//   projectIntakeProcess: "",
+//   projectEngagementPreference: "",
+//   currency: "",
+//   discoveryCall: "",
+//   consultation: "",
+//   delivery: "",
+//   profilePicture: null,
+// };
 
 export const postMessagesInitialValues = {
   content: "",
   receiverId: "",
 };
 
-export const onboardingInnovatorsInitialValues={
+export const onboardingExpertsInitialValues = {
+  personalInfo: {
+    firstName: "",
+    lastName: "",
+    country: "",
+    city: "",
+    currentPosition: "",
+  },
+  professionalInfo: {
+    areaOfExpertise:"",
+    canDo: "",
+    goals: "",
+  },
+  experience: {
+    compensationOption: "",
+    projSelectionOption: "",
+    projIntakeProcess: [],
+  },
+  rate: {
+    projEngagementPref: "",
+    currency: "",
+    discoverCall: "",
+    consultation: "",
+    delivery: "",
+  },
 
-firstName:"",
-lastName:"",
-dateOfBirth:"",
-gender:"",
-country:"",
-city:"",
-mybackground:"",
-currentposition:"",
-skills:"",
-startupfounder:"",
-community:"",
-communityevent:"",
-startup:""
-}
+  profilePicture:null
+
+};
+
+export const onboardingInnovatorsInitialValues = {
+  firstName: "",
+  lastName: "",
+  dateOfBirth: "",
+  gender: "",
+  country: "",
+  city: "",
+  mybackground: "",
+  currentposition: "",
+  skills: "",
+  startupfounder: "",
+  community: "",
+  communityevent: "",
+  startup: "",
+};
