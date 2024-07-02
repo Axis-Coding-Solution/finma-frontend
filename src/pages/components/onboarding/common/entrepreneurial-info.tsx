160c82672233441a8184e2586409cffae5a7ade4
Mubeen Shafiq <muhammadmubeen9182@gmail.com>
2024-07-02 17:56:37 +0500

    checked the onboarding innovators design

--- src/pages/components/onboarding/common/entrepreneurial-info.tsx
+++ src/pages/components/onboarding/common/entrepreneurial-info.tsx
@@ -35,7 +35,7 @@ import {
   ProfessionalInfo,
 } from "@/pages/components/onboarding/innovators";
 export const EntrepreneurialInfo = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const {
     control,
     register,
@@ -45,118 +45,159 @@ export const EntrepreneurialInfo = () => {
     defaultValues: onboardingInnovatorsInitialValues,
   });
   return (
-    <div>
-      <div className="">
-        <SectionHeading heading="Your Entrepreneurial Track-Record" />
-        <div className="flex  gap-4 mt-4">
-          <div className="w-full">
-            <Label htmlFor="entrepreneurialTrackRecord.nameOfStratUp">
-              Name Of StartUp
+    <div className="pt-5 w-full flex flex-col gap-4">
+      <SectionHeading heading="Your Entrepreneurial Track-Record" />
+      <div className="flex gap-4">
+        <div className="w-full">
+          <Label htmlFor="entrepreneurialTrackRecord.nameOfStratUp">
+            Name Of StartUp
+          </Label>
+          <Input
+            type="text"
+            id="entrepreneurialTrackRecord.nameOfStratUp"
+            {...register("entrepreneurialTrackRecord.nameOfStratUp")}
+          />
+          <InputError
+            error={errors.entrepreneurialTrackRecord?.nameOfStratUp}
+          />
+        </div>
+
+        <div className="w-full">
+          <Label htmlFor="entrepreneurialTrackRecord.industry">Industry</Label>
+          <Controller
+            name="entrepreneurialTrackRecord.industry"
+            control={control}
+            render={({ field }) => (
+              <Select
+                value={field.value}
+                onValueChange={(e) => field.onChange(e)}
+              >
+                <SelectTrigger id="entrepreneurialTrackRecord.industry">
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  {industry.map((item) => (
+                    <SelectItem key={item} value={item}>
+                      {item}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          ></Controller>
+          <InputError error={errors.entrepreneurialTrackRecord?.industry} />
+        </div>
+      </div>
+
+      <div className="w-full">
+        <Label htmlFor="entrepreneurialTrackRecord.aboutTheStartup">
+          About the startup (120 letter max)
+        </Label>
+        <Input
+          type="text"
+          id="entrepreneurialTrackRecord.aboutTheStartup"
+          {...register("entrepreneurialTrackRecord.aboutTheStartup")}
+        />
+        <InputError
+          error={errors.entrepreneurialTrackRecord?.aboutTheStartup}
+        />
+      </div>
+
+      <div className="flex gap-4">
+        <div className="w-full">
+          <Label htmlFor="entrepreneurialTrackRecord.yourRole">
+            Your Role
+          </Label>
+          <Input
+            type="text"
+            id="entrepreneurialTrackRecord.yourRole"
+            {...register("entrepreneurialTrackRecord.yourRole")}
+          />
+          <InputError error={errors.entrepreneurialTrackRecord?.yourRole} />
+        </div>
+        <div className="w-full">
+          <Label htmlFor="  entrepreneurialTrackRecord.websiteLink">
+            Website link
+          </Label>
+          <Input
+            type="text"
+            id="  entrepreneurialTrackRecord.websiteLink"
+            {...register("entrepreneurialTrackRecord.websiteLink")}
+          />
+          <InputError error={errors.entrepreneurialTrackRecord?.websiteLink} />
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className="w-full lg:w-[60%] ">
+          <Label htmlFor="  entrepreneurialTrackRecord.numberOfEmployee">
+            Number of employees
+          </Label>
+          <Input
+            type="text"
+            id="  entrepreneurialTrackRecord.numberOfEmployee"
+            {...register("entrepreneurialTrackRecord.numberOfEmployee")}
+          />
+          <InputError
+            error={errors.entrepreneurialTrackRecord?.numberOfEmployee}
+          />
+        </div>
+
+        <div className="flex flex-col w-full lg:w-[40%] ">
+          <div>
+            <Label htmlFor="yearsOfOperations">
+              Years of operations (from-to)
             </Label>
-            <Input
-              type="text"
-              id="entrepreneurialTrackRecord.nameOfStratUp"
-              {...register("entrepreneurialTrackRecord.nameOfStratUp")}
-            />
-            <InputError
-              error={errors.entrepreneurialTrackRecord?.nameOfStratUp}
-            />
           </div>
 
-          <div className="w-full">
-            <Label htmlFor="entrepreneurialTrackRecord.industry">
-              Industry
-            </Label>
+          <div className="flex gap-4">
             <Controller
-              name="entrepreneurialTrackRecord.industry"
+              name="entrepreneurialTrackRecord.yearsOfOperations"
               control={control}
               render={({ field }) => (
-                <Select
+                <DatePicker
+                  showOutsideDays={false}
+                  onChange={field.onChange}
+                  onBlur={field.onBlur}
                   value={field.value}
-                  onValueChange={(e) => field.onChange(e)}
-                >
-                  <SelectTrigger id="entrepreneurialTrackRecord.industry">
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {industry.map((item) => (
-                      <SelectItem key={item} value={item}>
-                        {item}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
+                />
               )}
-            ></Controller>
-            <InputError error={errors.entrepreneurialTrackRecord?.industry} />
-          </div>
-        </div>
-
-        <div className="w-full mt-4">
-          <Label htmlFor="  entrepreneurialTrackRecord.aboutTheStartup">
-            About the startup (120 letter max)
-          </Label>
-          <Input
-            type="text"
-            id="  entrepreneurialTrackRecord.aboutTheStartup"
-            {...register("entrepreneurialTrackRecord.aboutTheStartup")}
-          />
-          <InputError
-            error={errors.entrepreneurialTrackRecord?.aboutTheStartup}
-          />
-        </div>
-
-        <div className="flex gap-4">
-          <div className="w-full mt-4">
-            <Label htmlFor="  entrepreneurialTrackRecord.yourRole">
-              Your Role
-            </Label>
-            <Input
-              type="text"
-              id="  entrepreneurialTrackRecord.yourRole"
-              {...register("entrepreneurialTrackRecord.yourRole")}
             />
-            <InputError error={errors.entrepreneurialTrackRecord?.yourRole} />
-          </div>
-          <div className="w-full mt-4">
-            <Label htmlFor="  entrepreneurialTrackRecord.websiteLink">
-              Website link{" "}
-            </Label>
-            <Input
-              type="text"
-              id="  entrepreneurialTrackRecord.websiteLink"
-              {...register("entrepreneurialTrackRecord.websiteLink")}
-            />
-            <InputError
-              error={errors.entrepreneurialTrackRecord?.websiteLink}
+            <Controller
+              name="entrepreneurialTrackRecord.yearsOfOperations"
+              control={control}
+              render={({ field }) => (
+                <DatePicker
+                  showOutsideDays={false}
+                  onChange={field.onChange}
+                  onBlur={field.onBlur}
+                  value={field.value}
+                />
+              )}
             />
           </div>
         </div>
+      </div>
 
-        <div className="flex flex-col lg:flex-row mt-4 gap-4">
-          <div className="w-full lg:w-[60%] ">
-            <Label htmlFor="  entrepreneurialTrackRecord.numberOfEmployee">
-              Number of employees{" "}
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className=" flex flex-col gap-2 w-full">
+          <div>
+            <Label htmlFor="entrepreneurialTrackRecord.lastYearRevenue">
+              Last Year Revenue
             </Label>
-            <Input
-              type="text"
-              id="  entrepreneurialTrackRecord.numberOfEmployee"
-              {...register("entrepreneurialTrackRecord.numberOfEmployee")}
-            />
-            <InputError
-              error={errors.entrepreneurialTrackRecord?.numberOfEmployee}
-            />
           </div>
 
-          <div className="flex flex-col w-full lg:w-[40%] ">
-            <div>
-              <Label htmlFor="yearsOfOperations">
-                {" "}
-                Years of operations (from-to)
-              </Label>
-            </div>
-
+          <div>
             <div className="flex gap-4">
+              <Input
+                type="text"
+                id="entrepreneurialTrackRecord.lastYearRevenue"
+                {...register("entrepreneurialTrackRecord.lastYearRevenue")}
+              />
+              <InputError
+                error={errors.entrepreneurialTrackRecord?.lastYearRevenue}
+              />
+
               <Controller
                 name="entrepreneurialTrackRecord.yearsOfOperations"
                 control={control}
@@ -169,6 +210,27 @@ export const EntrepreneurialInfo = () => {
                   />
                 )}
               />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <div>
+            <Label htmlFor="entrepreneurialTrackRecord.lastYearRevenue">
+              Last Year Revenue
+            </Label>
+          </div>
+
+          <div>
+            <div className="flex gap-4">
+              <Input
+                type="text"
+                id="entrepreneurialTrackRecord.lastYearRevenue"
+                {...register("entrepreneurialTrackRecord.lastYearRevenue")}
+              />
+              <InputError
+                error={errors.entrepreneurialTrackRecord?.lastYearRevenue}
+              />
+
               <Controller
                 name="entrepreneurialTrackRecord.yearsOfOperations"
                 control={control}
@@ -184,103 +246,32 @@ export const EntrepreneurialInfo = () => {
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="flex flex-col lg:flex-row gap-4">
-          <div className=" flex flex-col gap-2 w-full mt-4">
-            <div>
-              <Label htmlFor="  entrepreneurialTrackRecord.lastYearRevenue">
-                Last Year Revenue
-              </Label>
-            </div>
-
-            <div>
-              <div className="flex gap-4">
-                <Input
-                  type="text"
-                  id="  entrepreneurialTrackRecord.lastYearRevenue"
-                  {...register("entrepreneurialTrackRecord.lastYearRevenue")}
-                />
-                <InputError
-                  error={errors.entrepreneurialTrackRecord?.lastYearRevenue}
-                />
-
-                <Controller
-                  name="entrepreneurialTrackRecord.yearsOfOperations"
-                  control={control}
-                  render={({ field }) => (
-                    <DatePicker
-                      showOutsideDays={false}
-                      onChange={field.onChange}
-                      onBlur={field.onBlur}
-                      value={field.value}
-                    />
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-          <div className=" flex flex-col gap-2 w-full mt-4">
-            <div>
-              <Label htmlFor="  entrepreneurialTrackRecord.lastYearRevenue">
-                Last Year Revenue
-              </Label>
-            </div>
-
-            <div>
-              <div className="flex gap-4">
-                <Input
-                  type="text"
-                  id="  entrepreneurialTrackRecord.lastYearRevenue"
-                  {...register("entrepreneurialTrackRecord.lastYearRevenue")}
-                />
-                <InputError
-                  error={errors.entrepreneurialTrackRecord?.lastYearRevenue}
-                />
-
-                <Controller
-                  name="entrepreneurialTrackRecord.yearsOfOperations"
-                  control={control}
-                  render={({ field }) => (
-                    <DatePicker
-                      showOutsideDays={false}
-                      onChange={field.onChange}
-                      onBlur={field.onBlur}
-                      value={field.value}
-                    />
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-full mt-4">
-          <Label htmlFor="  entrepreneurialTrackRecord.accomplishment">
-            Accomplishment
-          </Label>
-          <Input
-            type="text"
-            id="  entrepreneurialTrackRecord.accomplishment"
-            {...register("entrepreneurialTrackRecord.accomplishment")}
-          />
-          <InputError
-            error={errors.entrepreneurialTrackRecord?.accomplishment}
-          />
-        </div>
-        <div className="w-full mt-4">
-          <Label htmlFor="  entrepreneurialTrackRecord.companyLinkedIn">
-            Company LinkedIn
-          </Label>
-          <Input
-            type="text"
-            id="  entrepreneurialTrackRecord.companyLinkedIn"
-            {...register("entrepreneurialTrackRecord.companyLinkedIn")}
-            placeholder="https://www.linkedin.com/"
-          />
-          <InputError
-            error={errors.entrepreneurialTrackRecord?.companyLinkedIn}
-          />
-        </div>
+      <div className="w-full">
+        <Label htmlFor="entrepreneurialTrackRecord.accomplishment">
+          Accomplishment
+        </Label>
+        <Input
+          type="text"
+          id="entrepreneurialTrackRecord.accomplishment"
+          {...register("entrepreneurialTrackRecord.accomplishment")}
+        />
+        <InputError error={errors.entrepreneurialTrackRecord?.accomplishment} />
+      </div>
+      <div className="w-full">
+        <Label htmlFor="entrepreneurialTrackRecord.companyLinkedIn">
+          Company LinkedIn
+        </Label>
+        <Input
+          type="text"
+          id="entrepreneurialTrackRecord.companyLinkedIn"
+          {...register("entrepreneurialTrackRecord.companyLinkedIn")}
+          placeholder="https://www.linkedin.com/"
+        />
+        <InputError
+          error={errors.entrepreneurialTrackRecord?.companyLinkedIn}
+        />
       </div>
     </div>
   );
--- src/pages/components/onboarding/innovators/community-info.tsx
+++ src/pages/components/onboarding/innovators/community-info.tsx
@@ -1,4 +1,8 @@
-import { communityGoals, communityGoalsOptions, startUpFounder } from "@/data/dashboard/innovators";
+import {
+  communityGoals,
+  communityGoalsOptions,
+  startUpFounder,
+} from "@/data/dashboard/innovators";
 
 import { SectionHeading } from "@/pages/components/common";
 import { Label } from "@/components/ui/label";
@@ -27,104 +31,101 @@ export const CommunityInfo = () => {
     defaultValues: onboardingInnovatorsInitialValues,
   });
   return (
-    <div>
-      <div className="">
-        <SectionHeading heading="COMMUNITY INFORMATION" />
+    <div className="w-full pt-5 flex flex-col gap-4">
+      <SectionHeading heading="COMMUNITY INFORMATION" />
+      <div className="grid grid-cols-1 gap-4">
+        <div>
+          <Label htmlFor="communityInformation.entrepreneurialStage">
+            What best describes your entrepreneurial stage?
+          </Label>
+          <Controller
+            name="communityInformation.entrepreneurialStage"
+            control={control}
+            render={({ field }) => (
+              <Select
+                value={field.value}
+                onValueChange={(e) => field.onChange(e)}
+              >
+                <SelectTrigger id="communityInformation.entrepreneurialStage">
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  {startUpFounder.map((item) => (
+                    <SelectItem key={item.heading} value={item.heading}>
+                      <div className="flex flex-col">
+                        <div className="font-bold">{item.heading}</div>
+                        <div>{item.subheading}</div>
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          ></Controller>
+          <InputError
+            error={errors.communityInformation?.entrepreneurialStage}
+          />
+        </div>
 
-        <div className="grid grid-cols-1 gap-4 mt-4">
-          <div>
-            <Label htmlFor="communityInformation.entrepreneurialStage">
-              What best describes your entrepreneurial stage?
-            </Label>
-            <Controller
-              name="communityInformation.entrepreneurialStage"
-              control={control}
-              render={({ field }) => (
-                <Select
-                  value={field.value}
-                  onValueChange={(e) => field.onChange(e)}
-                >
-                  <SelectTrigger id="communityInformation.entrepreneurialStage">
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {startUpFounder.map((item) => (
-                      <SelectItem key={item.heading} value={item.heading}>
-                        <div className="flex flex-col">
-                          <div className="font-bold">{item.heading}</div>
-                          <div>{item.subheading}</div>
-                        </div>
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              )}
-            ></Controller>
-            <InputError
-              error={errors.communityInformation?.entrepreneurialStage}
-            />
-          </div>
+        <div>
+          <Label htmlFor="communityInformation.communityGoals">
+            My community goals are
+          </Label>
+          <Controller
+            name="communityInformation.communityGoals"
+            control={control}
+            render={({ field }) => (
+              // <Select
+              //   value={field.value}
+              //   onValueChange={(e) => field.onChange(e)}
+              // >
+              //   <SelectTrigger id="communityInformation.communityGoals">
+              //     <SelectValue />
+              //   </SelectTrigger>
+              //   <SelectContent>
+              //     {communityGoals.map((item) => (
+              //       <SelectItem key={item.label} value={item.label}>
+              //         {item.checkbox} {item.label}
+              //       </SelectItem>
+              //     ))}
+              //   </SelectContent>
+              // </Select>
 
-          <div>
-            <Label htmlFor="communityInformation.communityGoals">
-              My community goals are
-            </Label>
-            <Controller
-              name="communityInformation.communityGoals"
-              control={control}
-              render={({ field }) => (
-                // <Select
-                //   value={field.value}
-                //   onValueChange={(e) => field.onChange(e)}
-                // >
-                //   <SelectTrigger id="communityInformation.communityGoals">
-                //     <SelectValue />
-                //   </SelectTrigger>
-                //   <SelectContent>
-                //     {communityGoals.map((item) => (
-                //       <SelectItem key={item.label} value={item.label}>
-                //         {item.checkbox} {item.label}
-                //       </SelectItem>
-                //     ))}
-                //   </SelectContent>
-                // </Select>
-
-                <ReactSelect
+              <ReactSelect
                 {...field}
                 isMulti
                 options={communityGoalsOptions}
                 isClearable
               />
-              )}
-            ></Controller>
-            <InputError error={errors.communityInformation?.communityGoals} />
-          </div>
+            )}
+          ></Controller>
+          <InputError error={errors.communityInformation?.communityGoals} />
+        </div>
 
-          <div>
-            <Label htmlFor="communityInformation.personalBio">
-              Personal bio (150 character only){" "}
-            </Label>
-            <Input
-              type="text"
-              id="communityInformation.personalBio"
-              {...register("communityInformation.personalBio")}
-              // placeholder="Enter first name"
-            />
-            <InputError error={errors.communityInformation?.personalBio} />
-          </div>
-          <div>
-            <Label htmlFor="communityInformation.dedicateToYourVenture">
-              How many hours per weeks can you dedicate to your venture(s)?
-            </Label>
-            <Input
-              type="text"
-              id="communityInformation.dedicateToYourVenture"
-              {...register("communityInformation.dedicateToYourVenture")}
-            />
-            <InputError
-              error={errors.communityInformation?.dedicateToYourVenture}
-            />
-          </div>
+        <div>
+          <Label htmlFor="communityInformation.personalBio">
+            Personal bio (150 character only){" "}
+          </Label>
+          <Input
+            type="text"
+            id="communityInformation.personalBio"
+            {...register("communityInformation.personalBio")}
+            // placeholder="Enter first name"
+          />
+          <InputError error={errors.communityInformation?.personalBio} />
+        </div>
+        <div>
+          <Label htmlFor="communityInformation.dedicateToYourVenture">
+            How many hours per weeks can you dedicate to your venture(s)?
+          </Label>
+          <Input
+            type="text"
+            id="communityInformation.dedicateToYourVenture"
+            {...register("communityInformation.dedicateToYourVenture")}
+          />
+          <InputError
+            error={errors.communityInformation?.dedicateToYourVenture}
+          />
         </div>
       </div>
     </div>
