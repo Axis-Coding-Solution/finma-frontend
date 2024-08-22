8993f312b883ccff007bce0842bfc40e24de63f6
Your New Name <huzaifaak5050@gmail.com>
2024-08-22 19:49:11 +0500

    make profile and detail

--- src/pages/components/profile/profile-entrepreneurial.tsx
+++ src/pages/components/profile/profile-entrepreneurial.tsx
@@ -25,7 +25,7 @@ export const ProfileEntrepreneurial = () => {
       <p className="text-foreground 2xl:text-2xl text-lg 2xl:leading-7 leading-6">
         Select your entrepreneurial stage
       </p>
-      <div className="custom-scrollbar flex flex-col 2xl:gap-6 gap-4 2xl:mt-10 mt-6 2xl:h-[240px] h-[180px] overflow-y-auto px-0.5 2xl:py-4 py-2">
+      <div className="custom-scrollbar-warning flex flex-col 2xl:gap-6 gap-4 2xl:mt-10 mt-6 2xl:h-[240px] h-[180px] overflow-y-auto px-0.5 2xl:py-4 py-2">
         {labelOptions &&
           labelOptions.map((item) => (
             <div className="flex gap-3 items-start">
