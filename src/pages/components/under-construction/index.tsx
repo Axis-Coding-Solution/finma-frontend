15669112d13021e63d3ad4a092fc2256447f6604
Mubeen Shafiq <mubeenshafiq@axiscodingsolutions.com>
2024-09-25 13:21:26 +0500

    idea validation

--- src/pages/components/under-construction/index.tsx
+++ src/pages/components/under-construction/index.tsx
@@ -1,13 +1,25 @@
 import { Button } from "@/components/_ui/button";
 import { Construction } from "lucide-react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const UnderConstruction = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    if (window.history.state.idx > 0) {
+      return navigate(-1);
+    }
+    return navigate("/dashboard/community");
+  };
+
   return (
-<div className="flex flex-col gap-2 justify-start items-center h-full">
+    <div className="flex flex-col gap-2 justify-start items-center h-full">
       <Construction size={96} className="text-amber-500" />
       <h2 className="text-4xl font-bold">Under Construction</h2>
-      <Button className="mt-5" tag={Link} to="/dashboard/community" variant="dark">Go Back</Button>
-    </div>  )
-}
+      <Button className="mt-5" onClick={handleGoBack} variant="dark">
+        Go Back
+      </Button>
+    </div>
+  );
+};
 
-export default UnderConstruction
+export default UnderConstruction;
